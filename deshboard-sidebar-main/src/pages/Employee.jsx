f1d77f8ee2426471e8286c395a0b724fe943b8f2
TMadhusha <122914674+TMadhusha@users.noreply.github.com>
2024-03-20 08:36:37 +0530

    last

--- deshboard-sidebar-main/src/pages/Employee.jsx
+++ deshboard-sidebar-main/src/pages/Employee.jsx
@@ -32,11 +32,69 @@ const Employee = () => {
                 {showEmployeeDetails && (
                     <div className="employee-details">
                        <h3>Employee details</h3>
+                       <table border={'1'}
+                       cellPadding={'1'}
+                       cellSpacing={4}>
+                        
+                        <tr>
+                            <th>Employee ID</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>DOB</th>
+                            <th>Address</th>
+                            <th>NIC</th>
+                            <th>Email</th>
+                            <th>Phone No</th>
+                            <th>Role</th>
+                            <th>Image</th>
+                        </tr>
+                       </table>
+                       <div>
+                       <button className='btnemp' >Add</button>
+                       <button className='btnemp' >Update</button>
+                       <button className='btnemp' >Delete</button>
+                       </div>
+                       
+
                     </div>
                 )}
                 {showAttendanceDetails && (
                     <div className="employee-details">
-                       <h3>Attandance details</h3>
+                       <div>
+                        <h3>Add Attendance</h3>
+                        </div>
+                        <div>
+                                <form>
+                                <table >
+                                    <tr>
+                                        <th>Attendance ID:</th>
+                                        <td><input type='text' name='attid' value='attid'/></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Employee ID:</th>
+                                        <td><input type='text' name='empid' value='empid'/></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Date:</th>
+                                        <td><input type='text' name='date' value='date'/></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Check In:</th>
+                                        <td><input type='text' name='checkin' value='checkin'/></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Check out:</th>
+                                        <td><input type='text' name='checkout' value='checkout'/></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Status:</th>
+                                        <td><input type='text' name='status' value='status'/></td>
+                                    </tr>
+                                    </table>
+                                    <button>Add</button>
+                                    <button>Cancel</button>
+                                </form>
+                        </div>
                     </div>
                 )}
                 {showSalaryDetails && (
