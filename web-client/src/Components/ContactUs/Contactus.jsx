016a059bfb95a311029aeef7ac39495327d323d5
Abdullah Asfaque <112546546+Abdasfaque@users.noreply.github.com>
2024-03-23 13:23:50 +0530

    Update

--- web-client/src/Components/ContactUs/Contactus.jsx
+++ web-client/src/Components/ContactUs/Contactus.jsx
@@ -3,9 +3,7 @@ import './contactus.css'
 import ReCAPTCHA from 'react-google-recaptcha';
 //6Le35JUpAAAAAFFt9Ngrj0RBJSUfYfKN8wAmj0c1
 
-const Contactus = () => {
-
-    
+const Contactus = () => { 
 
   return (
     <section>
