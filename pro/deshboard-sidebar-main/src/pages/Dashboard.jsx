9cb9f34a8e0cad82422e5a2224577f683fd1890e
TMadhusha <122914674+TMadhusha@users.noreply.github.com>
2024-03-26 06:30:10 +0530

    last

--- pro/deshboard-sidebar-main/src/pages/Dashboard.jsx
+++ pro/deshboard-sidebar-main/src/pages/Dashboard.jsx
@@ -6,39 +6,34 @@ import Home from '../components/Home';
 
 const Dashboard = () => {
     return (
-        <main className='main-container backimg'>
+        <main className='main-container'>
         <div className='main-title ' >
             <h1 className='page-title'>DASHBOARD</h1>
         </div>
 
         <div className='main-cards'>
             <div className='card'>
+                <img src="employee.jpg" />           
+            </div>
+            <div><button>Employee</button></div>
+            <div className='card'>
                 <div className='card-inner'>
-                    <h3>Total Jewellery Type</h3>
-                    <FcPositiveDynamic className='card_icon'/>
+                    <h3>Customer</h3>
+                    
                 </div>
-                <h1>300</h1>
+                
             </div>
             <div className='card'>
                 <div className='card-inner'>
-                    <h3>Total stock</h3>
-                    <FcPositiveDynamic className='card_icon'/>
+                    
                 </div>
-                <h1>12</h1>
+                
             </div>
             <div className='card'>
                 <div className='card-inner'>
-                    <h3>Total Customers</h3>
-                    <FcPositiveDynamic className='card_icon'/>
+                   
+                    
                 </div>
-                <h1>33</h1>
-            </div>
-            <div className='card'>
-                <div className='card-inner'>
-                    <h3>Total Sale</h3>
-                    <FcPositiveDynamic className='card_icon'/>
-                </div>
-                <h1>42</h1>
             </div>
         </div>
     </main>
