2f8903837314bdd0a0ec1e50e9ac0f33c5d9b5cd
Tirasha <1999tirasha@gmail.com>
2024-03-30 19:29:53 +0530

    commit

--- pro/deshboard-sidebar-main/src/customer/ManageCx.jsx
+++ pro/deshboard-sidebar-main/src/customer/ManageCx.jsx
@@ -1,12 +1,52 @@
-import React, { PureComponent } from 'react'
-import CustomerBar from '../components/CustomerBar'
+import React, { useEffect, useState } from 'react';
+import CustomerBar from '../components/CustomerBar';
+import Table from 'react-bootstrap/Table';
+import axios from 'axios';
 
-export default class ManageCx extends PureComponent {
-  render() {
-    return (
-      <CustomerBar>
-        <h1>Manage customer</h1>
-        </CustomerBar>
-    )
-  }
+function ManageCx() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    console.log("Tirasha");
+  });
+
+  return (
+    <CustomerBar>
+      <div className='container'>
+        <div className='py-4'>
+          <Table className='table border shadow'>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td colSpan={2}>Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    </CustomerBar>
+  );
 }
+
+export default ManageCx;
