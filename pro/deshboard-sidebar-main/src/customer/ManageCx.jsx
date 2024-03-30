2830e1c3d4bc5f2c72f9717f71116ea7e62cb5d0
Tirasha <1999tirasha@gmail.com>
2024-03-30 22:17:17 +0530

    commit

--- pro/deshboard-sidebar-main/src/customer/ManageCx.jsx
+++ pro/deshboard-sidebar-main/src/customer/ManageCx.jsx
@@ -1,46 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import CustomerBar from '../components/CustomerBar';
 import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button'; // Import Button component
 import axios from 'axios';
+import '../customer/Customer.css';
 
 function ManageCx() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    console.log("Tirasha");
-  });
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/getcustomer");
+      setUsers(response.data); // Update the state with fetched data
+    } catch (error) {
+      console.error('Error loading users:', error);
+    }
+  }
 
   return (
     <CustomerBar>
+      <h1 className='table title'>Customer Details</h1>
+          <center></center><Button variant="primary" className="mr-2">Add Customer</Button>
+          <Button variant="info" className="mr-2">Update Customer</Button>
+          <Button variant="danger">Delete Customer</Button><br/><br/><center/>
       <div className='container'>
-        <div className='py-4'>
+        <div className='py-4 table-container'>
           <Table className='table border shadow'>
             <thead>
               <tr>
-                <th>#</th>
+                <th>Customer ID</th>
                 <th>First Name</th>
                 <th>Last Name</th>
-                <th>Username</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>Phone No</th>
+                <th>How They Heard About</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td colSpan={2}>Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
+              {users.map(user => (
+                <tr key={user.id}>
+                  <td>{user.cus_id}</td>
+                  <td>{user.firstname}</td>
+                  <td>{user.lastname}</td>
+                  <td>{user.email}</td>
+                  <td>{user.address}</td>
+                  <td>{user.phoneNo}</td>
+                  <td>{user.hearAbout}</td>
+                </tr>
+              ))}
             </tbody>
           </Table>
         </div>
