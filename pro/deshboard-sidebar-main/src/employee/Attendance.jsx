e5ca0a1728953b85449cfe584786e0764d41c096
TMadhusha <madhusha200069@gmail.com>
2024-04-11 14:38:10 +0530

    last

--- pro/deshboard-sidebar-main/src/employee/Attendance.jsx
+++ pro/deshboard-sidebar-main/src/employee/Attendance.jsx
@@ -4,11 +4,23 @@ import '../css/employee.css';
 import { FaSearch } from "react-icons/fa";
 import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
+import AddAttendance from './AddAttendance';
+import EditAttendance from './EditAttendance';
 
 export default function Attendance() {
   const [attendance,setAttendance]=useState([]);
   const [searchQuery,setSearchQuery]=useState('');
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleAddAttendanceClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+    
   const {att_id}=useParams();
 
   useEffect(()=>{
@@ -33,22 +45,24 @@ export default function Attendance() {
     alert("No search results found");
   }
 
-
-
   return (
     <div className='container'>
       <EmployeeBar>
         <div className='main-container'>
-          <div className='section'>
-            <div>
           <div className='main-title'>
           <h2>Attendance</h2>
           </div>
-          <div className='search-bar-container'>
+          <div className='searchAdd-container section'>
+            <div className='addAtt'>
+            <button className='btn' onClick={handleAddAttendanceClick}> Add Attendance </button>
+            {isModalOpen && <AddAttendance onClose={handleCloseModal} />}
+            </div>
+            <div className='search-bar-container'>
             <FaSearch className='search-icon' />
             <input type='text' placeholder='Search here...' className='search-input' value={searchQuery} onChange={handleSearchInputChange}/>
+            </div>
           </div>
-          <div>
+          <div className='tab'>
           <table class="table">
             <thead>
              <tr>
@@ -69,53 +83,17 @@ export default function Attendance() {
                     <td>{attendance.date}</td>
                     <td>{attendance.check_In}</td>
                     <td>{attendance.check_Out}</td>
-                    <td><Link className='small-button' to={'/addattendance'}> Add </Link></td>
-                    <td><button className='small-button'>Update</button></td>
+                    <td><button className='small-button' onClick={handleAddAttendanceClick}>Update</button>
+                    {isModalOpen && <EditAttendance onClose={handleCloseModal} />}
+                    </td>
+
                   </tr>
                 ))
-              }
+                }
             </tbody>
           </table>
           </div>
-          </div>
-          <div>
-            <div>
-            <h2>Add Attendance</h2>
-            </div>
-            <div>
-              <form className='form'>
-                <table>
-                  <tr>
-                    <th><label>Employee ID: </label></th>
-                    <td><input type={'text'} name='emp_id'  placeholder={'Employee ID'}/></td>
-                  </tr>
-                  <tr>
-                    <th><label>Attendance ID: </label></th>
-                    <td><input type={'text'} name='att_id'  placeholder={'Attendance ID'}/></td>
-                  </tr>
-                  <tr>
-                    <th><label>Date: </label></th>
-                    <td><input type={'text'} name='date'  placeholder={'Date'}/></td>
-                  </tr>
-                  <tr>
-                    <th><label>Check In: </label></th>
-                    <td><input type={'text'} name='checkIn'  placeholder='Check In'/></td>
-                  </tr>
-                  <tr>
-                    <th><label>Check Out: </label></th>
-                    <td><input type={'text'} name='checkOut'  placeholder='Check Out'/></td>
-                  </tr>
-                  <tr>
-                    <td><button className='small-button'>Add</button></td>
-                    <td><button className='small-button'>Cancel</button></td>
-                  </tr>
-                </table>
-              </form>
-            </div>
-
-          </div>
-          </div>
-        </div>
+        </div>  
       </EmployeeBar>
     </div>
   )
