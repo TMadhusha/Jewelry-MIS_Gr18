f71db5965896d828c841434751655bbd76c15b66
AktharZaem <117557523+AktharZaem@users.noreply.github.com>
2024-04-16 10:04:13 +0530

    supplier

---
+++ pro/deshboard-sidebar-main/src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import EmployeeBar from './components/EmployeeBar.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+import Customer from './pages/Customer.jsx';
+import Employee from './pages/Employee.jsx';
+import Inventory from './pages/Inventory.jsx';
+import Supplier from './pages/Supplier.jsx';
+import Finance from './pages/Finance.jsx';
+import More from './pages/More.jsx';
+import Logout from './pages/Logout.jsx';
+import AddEmp from './employee/AddEmp.jsx';
+import EditEmp from './employee/EditEmp.jsx';
+
+
+function App ()  {
+  return ( 
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Dashboard />} />
+    <Route path="/employee" element={<Employee />} />
+    <Route path="/customer" element={<Customer />} />
+    <Route path="/inventory" element={<Inventory />} />
+    <Route path="/supplier" element={<Supplier />} />
+    <Route path="/finance" element={<Finance />} />
+    <Route path="/more" element={<More />} />
+    <Route path="/addemp" element={<AddEmp/>}/>
+    <Route path="/editemp/:emp_id" element={<EditEmp />} />
+    
+    <Route path="/logout" element={<Logout/>}/>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
