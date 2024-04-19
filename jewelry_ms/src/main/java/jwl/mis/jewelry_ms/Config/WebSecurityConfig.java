0ed4266c4a1ca71030bfc1d77e7c705fe962f763
AktharZaem <117557523+AktharZaem@users.noreply.github.com>
2024-04-20 01:49:03 +0530

    afterBasic

--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/Config/WebSecurityConfig.java
+++ jewelry_ms/src/main/java/jwl/mis/jewelry_ms/Config/WebSecurityConfig.java
@@ -3,9 +3,12 @@ package jwl.mis.jewelry_ms.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @EnableWebSecurity
 @Configuration
@@ -15,4 +18,19 @@ public class WebSecurityConfig {
 
         return new BCryptPasswordEncoder(11);
     }
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+//        http
+//                .cors()
+//                .and()
+//                .csrf()
+//                .disable()
+//                .authorizeHttpRequests()
+//                .dispatcherTypeMatchers().permitAll()
+//    }
+
 }
+
+
+
+
