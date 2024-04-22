e04a1e805fc9d75192b00c7af4781b09f4a47be8
AktharZaem <117557523+AktharZaem@users.noreply.github.com>
2024-04-22 22:04:52 +0530

    failedSecurity

--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/Config/WebSecurityConfigurerAdapter.java
+++
@@ -1,4 +0,0 @@
-package jwl.mis.jewelry_ms.Config;
-
-public class WebSecurityConfigurerAdapter {
-}
--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/repository/VerificationTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package jwl.mis.jewelry_ms.repository;
-
-import jwl.mis.jewelry_ms.model.VerificationToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public abstract class VerificationTokenRepository implements JpaRepository<VerificationToken, Long> {
-}
