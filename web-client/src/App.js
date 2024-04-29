7763b435cf78997b69ff928daa4c887fb0802659
Abdullah Asfaque <112546546+Abdasfaque@users.noreply.github.com>
2024-04-29 21:03:35 +0530

    New

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import './app.css'
-import Home from './Components/Home/Home'
 import Navbar from './Components/Navbar/Navbar'
 import Footer from './Components/Footer/Footer'
+import ContactUs from './Pages/ContactUs'
+import Hoem from './Pages/Hoem'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
-    <Navbar />
-    <Home/>
-    <Footer />
+    // <>
+    // <Navbar />
+    // <Home/>
+    // <Footer />
     
-    </>
+    // </>
+    <BrowserRouter>
+    <Navbar/>
+      <Route path='/hoem' element={<Hoem/>}/>
+      <Route path="/contactus" element={<ContactUs/>}/>
+    </BrowserRouter>
   )
 }
 
