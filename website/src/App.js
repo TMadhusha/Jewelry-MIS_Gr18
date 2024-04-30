a107ca3400b7c66b89f0d95a948a4e63618f8bf4
Abdullah Asfaque <112546546+Abdasfaque@users.noreply.github.com>
2024-04-30 14:47:57 +0530

    New

--- website/src/App.js
+++ website/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './Pages/Home';
+import ContactUs from './Pages/ContactUs';
+import HomeWrapper from './Pages/HomeWrapper';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      Website
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeWrapper />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/contactus" element={<ContactUs />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ website/src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import logo from '../Images/Slide.png';
+
+
+export default function Home() {
+  return (
+    <section className='HomeSection'>
+          <div className="banner">
+             <a href="" className="logo flex">
+              <img src={logo} alt='' className="logo flex"/>
+            </a>
+         </div>
+
+      </section>
+  )
+}
