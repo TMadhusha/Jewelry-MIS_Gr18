28ea1177282775c5c7b4776a804dc49fffe576ac
TMadhusha <madhusha200069@gmail.com>
2024-08-14 11:12:59 +0530

    changes

--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/dto/SupplierUpdateDto.java
+++
@@ -1,8 +0,0 @@
-package jwl.mis.jewelry_ms.dto;
-
-import lombok.Data;
-
-@Data
-public class SupplierUpdateDto {
-    private String suppName;
-}
--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package jwl.mis.jewelry_ms.repository;
-
-
-import jwl.mis.jewelry_ms.model.Employee;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository <Employee,Long>{
-}
