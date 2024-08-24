5b1aa65a46637cb3bb632f20d03cad0863495416
Tirasha <1999tirasha@gmail.com>
2024-08-24 15:00:08 +0530

    c

--- jewelry_ms/src/main/java/jwl/mis/jewelry_ms/exeption/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package jwl.mis.jewelry_ms.exeption;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long sup_id){
-        super(sup_id+" "+"Is not Valid...");
-    }
-}
